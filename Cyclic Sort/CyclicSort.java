8b6cd2491e0f1ccc526b27882647fab13bf8c57e
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2024-07-30 19:09:00 +0530

    Updated CyclicSort.java

--- Cyclic Sort/CyclicSort.java
+++ Cyclic Sort/CyclicSort.java
@@ -1,9 +1,16 @@
 public class CyclicSort {
 
+    /*
+     * Identification
+     *
+     * The problem will involve an array of ‘n’ numbers containing numbers in the range of 1 to ‘n’.
+     *
+     * */
+
     public void sort(int[] nums) {
 
         int i = 0;
-        while(i < nums.length) {
+        while (i < nums.length) {
             int correctIndex = nums[i] - 1;
             if (nums[i] != nums[correctIndex]) {
                 swap(nums, i, correctIndex);
