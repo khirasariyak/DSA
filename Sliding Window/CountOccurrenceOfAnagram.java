9be0ca10e73a4135ee492d746c28db07b1b73e60
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2024-08-01 09:55:05 +0530

    Added CountOccurrenceOfAnagram.java

---
+++ Sliding Window/CountOccurrenceOfAnagram.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountOccurrenceOfAnagram {
+
+    public static void main(String[] args) {
+        String text = "forxxorfxdofr";
+        String pattern = "for";
+        System.out.println(countOccurrenceOfAnagram(text, pattern));
+    }
+
+    private static int countOccurrenceOfAnagram(String text, String pattern) {
+
+        int patternLength = pattern.length();
+        int textLength = text.length();
+
+        if (patternLength > textLength) {
+            return 0;
+        }
+
+        Map<Character, Integer> patternMap = createMap(pattern);
+        Map<Character, Integer> currentWindowMap = getInitialWindow(text, patternLength);
+        int count = 0;
+
+        if (patternMap.equals(currentWindowMap)) {
+            count++;
+        }
+
+        for (int i = 1; i < textLength - patternLength + 1; i++) {
+            char charToRemove = text.charAt(i - 1);
+            char charToAdd = text.charAt(i + patternLength - 1);
+
+            currentWindowMap.put(charToRemove, currentWindowMap.getOrDefault(charToRemove, 0) - 1);
+            currentWindowMap.put(charToAdd, currentWindowMap.getOrDefault(charToAdd, 0) + 1);
+
+            if (currentWindowMap.get(charToRemove) == 0) {
+                currentWindowMap.remove(charToRemove);
+            }
+
+            if (patternMap.equals(currentWindowMap)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static Map<Character, Integer> getInitialWindow(String text, int requiredLength) {
+        return createMap(text.substring(0, requiredLength));
+    }
+
+    private static Map<Character, Integer> createMap(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        return map;
+    }
+
+}
