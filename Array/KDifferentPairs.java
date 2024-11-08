984dbcca16a8967f7f4a034b539a3985af63a319
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2024-11-08 09:35:37 +0530

    Added KDifferentPairs.java

---
+++ Array/KDifferentPairs.java
@@ -0,0 +1,33 @@
+/*
+* https://leetcode.com/problems/k-diff-pairs-in-an-array/description/
+* */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KDifferentPairs {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 4, 1, 5};
+        int k = 2;
+        System.out.println(findPairs(nums, k));
+    }
+
+    private static int findPairs(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        int count = 0;
+        for (int i : map.keySet()) {
+            if (k == 0 && map.get(i) > 1 || k > 0 && map.containsKey(i + k)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
