af49bfa5acdd21941dd05a2dddf6589ab947e5c1
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2024-11-15 21:45:12 +0530

    Added CountingBits.java

---
+++ Bit Manipulation/CountingBits.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/*
+* https://leetcode.com/problems/counting-bits/
+* */
+
+public class CountingBits {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+
+    private static int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        result[0] = 0;
+
+        for (int i = 1; i < result.length; i++) {
+            if (i % 2 == 0) {
+                result[i] = result[i / 2];
+            } else {
+                result[i] = 1 + result[i / 2];
+            }
+        }
+        return result;
+    }
+
+}
