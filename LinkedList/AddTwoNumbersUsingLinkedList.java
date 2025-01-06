2dbc165dfd361fa00fea004f1321c93beecbcba2
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2025-01-07 02:14:44 +0530

    Added AddTwoNumbersUsingLinkedList.java

---
+++ LinkedList/AddTwoNumbersUsingLinkedList.java
@@ -0,0 +1,45 @@
+/*
+* https://leetcode.com/problems/add-two-numbers/description/
+* */
+
+public class AddTwoNumbersUsingLinkedList {
+
+    public static void main(String[] args) {
+        ListNode first = ListNode.getLinkedList();
+        ListNode second = ListNode.getLinkedList();
+        System.out.println(add(first, second, 0));
+    }
+
+    private static ListNode add(ListNode first, ListNode second, int carry) {
+
+        ListNode temp1 = first;
+        ListNode temp2 = second;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (temp1 != null || temp2 != null || carry != 0) {
+
+            int firstV = 0;
+            if (temp1 != null) {
+                firstV = temp1.val;
+                temp1 = temp1.next;
+            }
+
+            int secondV = 0;
+            if (temp2 != null) {
+                secondV = temp2.val;
+                temp2 = temp2.next;
+            }
+
+            int sum = firstV + secondV + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+            current.next = new ListNode(digit);
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+
+}
