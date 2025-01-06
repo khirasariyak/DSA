e94199b88c6bdedcb8028f778f66acb92b02cf19
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2025-01-07 01:46:36 +0530

    Added PalindromeLinkedList.java

---
+++ LinkedList/PalindromeLinkedList.java
@@ -0,0 +1,61 @@
+import static LinkedList.ListNode.getLinkedList;
+
+/*
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ * */
+
+public class PalindromeLinkedList {
+
+    public static void main(String[] args) {
+        ListNode list = getLinkedList();
+        System.out.println(isPalindromicLinkedList(list));
+    }
+
+    private static boolean isPalindromicLinkedList(ListNode head) {
+
+        ListNode mid = getMid(head);
+        ListNode rev = reverse(mid.next);
+
+        mid.next = null;
+
+        while(rev != null) {
+            if(head.val != rev.val) {
+                return false;
+            }
+            head = head.next;
+            rev = rev.next;
+        }
+
+        return true;
+    }
+
+    private static ListNode getMid(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static ListNode reverse(ListNode head) {
+
+        ListNode next;
+        ListNode prev = null;
+        ListNode current = head;
+
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+}
