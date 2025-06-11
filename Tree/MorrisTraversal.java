9e7ad0f815998be16f9367fb971c437882746b1c
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2025-06-11 09:08:31 +0530

    Added MorrisTraversal.java

---
+++ Tree/MorrisTraversal.java
@@ -0,0 +1,40 @@
+public class MorrisTraversal {
+
+    public static class TreeNode {
+        int value;
+        TreeNode left, right;
+
+        TreeNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public void morrisInorder(TreeNode root) {
+        TreeNode current = root;
+
+        while (current != null) {
+            if (current.left == null) {
+                // No left child, visit this node and move right
+                System.out.print(current.value + " ");
+                current = current.right;
+            } else {
+                // Find the inorder predecessor
+                TreeNode predecessor = current.left;
+                while (predecessor.right != null && predecessor.right != current) {
+                    predecessor = predecessor.right;
+                }
+
+                if (predecessor.right == null) {
+                    // Make a temporary thread to the current node
+                    predecessor.right = current;
+                    current = current.left;
+                } else {
+                    // Thread already exists, remove it and visit current
+                    predecessor.right = null;
+                    System.out.print(current.value + " ");
+                    current = current.right;
+                }
+            }
+        }
+    }
+}
