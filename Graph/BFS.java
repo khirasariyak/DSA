ecd19de263bf89ae5d0127152fe7e7a5a704640d
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2025-06-12 22:19:08 +0530

    Added BFS.java

---
+++ Graph/BFS.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+* https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+* */
+
+public class BFS {
+
+    public List<Integer> bfs(int V, List<List<Integer>> adj) {
+
+        boolean[] visited = new boolean[V];
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(0);
+        visited[0] = true;
+
+        while (!queue.isEmpty()) {
+
+            int node = queue.poll();
+            list.add(node);
+
+            for (int k : adj.get(node)) {
+                if (!visited[k]) {
+                    visited[k] = true;
+                    queue.add(k);
+                }
+            }
+
+        }
+
+        return list;
+    }
+
+}
