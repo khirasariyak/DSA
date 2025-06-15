b86ab3f0713ad7d63b2a0b746196095f9f76e263
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2025-06-15 22:05:17 +0530

    Added SubArraySumEqualsK.java

---
+++ Array/SubArraySumEqualsK.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * https://leetcode.com/problems/subarray-sum-equals-k/description/
+ * */
+
+public class SubArraySumEqualsK {
+
+    public static void main(String[] args) {
+        System.out.println(subArraySumEqualsK(new int[]{1, 1, 1}, 2));
+    }
+
+    public static int subArraySumEqualsK(int[] nums, int k) {
+
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        while (right < nums.length) {
+
+            sum += nums[right];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            right++;
+        }
+
+        return count;
+    }
+
+}
