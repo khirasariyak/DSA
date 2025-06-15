4aa02bc37f002e3db2c23b3858736fc8ad19c37c
Kishan Khirasariya <77135651+khirasariyak@users.noreply.github.com>
2025-06-15 20:40:44 +0530

    Updated TrappingRainWater.java

--- Stack/TrappingRainWater.java
+++ Stack/TrappingRainWater.java
@@ -67,4 +67,30 @@ public class TrappingRainWater {
         return sum;
     }
 
+    public int trapTwoPointer(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int waterTrapped = 0;
+    
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+    
+        return waterTrapped;
+    }
+
 }
